c6968f14e8a27a3ee102628f27ef52be4cfb8a32
Clemilson Azevedo <Clemilsondeazevedo@gmail.com>
2023-07-11 00:56:39 +0100

    actualizando o app para usar alerts

--- main.js
+++ main.js
@@ -5,13 +5,13 @@ alert("Hello World!")
 const sumTwoNumbers = (firstNum, secondNum) => firstNum + secondNum
 
 // Exercicio 3
-const verifyNumberType = (number) => typeof number == "number" ? "É um número" : "Não é um número"
+const verifyNumberType = (number) => typeof number == "number" ? alert("É um número") : alert("Não é um número")
 
 // Exercicio 4
-const verifyStringType = (str) => typeof str == "string" ? "É uma string" : "Não é uma string"
+const verifyStringType = (str) => typeof str == "string" ? alert("É uma string") : alert("Não é uma string")
 
 // Exercicio 5
-const verifyBoolType = (bool) => typeof bool == "boolean" ? "É um Booleano" : "Não é um Booleano"
+const verifyBoolType = (bool) => typeof bool == "boolean" ? alert("É um Booleano") : alert("Não é um Booleano")
 
 // Exercicio 6
 const subTwoNumbers = (firstNum, secondNum) => firstNum - secondNum
@@ -23,7 +23,7 @@ const mulTwoNumbers = (firstNum, secondNum) => firstNum * secondNum
 const divTwoNumbers = (firstNum, secondNum) => firstNum / secondNum
 
 // Exercicio 9
-const parOrNot = (number) => number % 2 == 0 ? "É um número par" : "Não é um número Par"
+const parOrNot = (number) => number % 2 == 0 ? alert("É um número par") : alert("Não é um número Par")
 
 // Exercicio 10
-const imparOrNot = (number) => number % 2 == 0 ? 'Não é um número impar' : "É um numero impar"
+const imparOrNot = (number) => number % 2 == 0 ? alert('Não é um número impar') : alert("É um numero impar")
