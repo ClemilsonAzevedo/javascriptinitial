1e069a3276f0ef220f12a593689413f4ffa58159
Clemilson Azevedo <Clemilsondeazevedo@gmail.com>
2023-07-11 00:46:47 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,29 @@
+// Exercicio 1
+alert("Hello World!")
+
+// Exercicio 2
+const sumTwoNumbers = (firstNum, secondNum) => firstNum + secondNum
+
+// Exercicio 3
+const verifyNumberType = (number) => typeof number == "number" ? "É um número" : "Não é um número"
+
+// Exercicio 4
+const verifyStringType = (str) => typeof str == "string" ? "É uma string" : "Não é uma string"
+
+// Exercicio 5
+const verifyBoolType = (bool) => typeof bool == "boolean" ? "É um Booleano" : "Não é um Booleano"
+
+// Exercicio 6
+const subTwoNumbers = (firstNum, secondNum) => firstNum - secondNum
+
+// Exercicio 7
+const mulTwoNumbers = (firstNum, secondNum) => firstNum * secondNum
+
+// Exercicio 8
+const divTwoNumbers = (firstNum, secondNum) => firstNum / secondNum
+
+// Exercicio 9
+const parOrNot = (number) => number % 2 == 0 ? "É um número par" : "Não é um número Par"
+
+// Exercicio 10
+const imparOrNot = (number) => number % 2 == 0 ? 'Não é um número impar' : "É um numero impar"
